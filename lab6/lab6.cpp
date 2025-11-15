e7a6dadf913ce22af3e16077fabf087c3876c373
Roman-Sarchuk <91730539+Roman-Sarchuk@users.noreply.github.com>
2025-11-15 20:04:26 +0200

    init lab6

---
+++ lab6/lab6.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <stdexcept>
+
+using namespace std;
+
+
+
+std::string readFileContent(const std::string& filePath);
+
+void writeFileContent(const std::string& filePath, const std::string& content);
+
+void toUpperCase(std::string& text);
+
+void toLowerCase(std::string& text);
+
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
+
+// --- auxiliary ---
+string readFileContent(const string& filePath) {
+    ifstream file(filePath);
+
+    if (!file.is_open())
+        throw runtime_error("Cannot open file to read: " + filePath);
+
+    return string((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
+}
+
+
+void writeFileContent(const string& filePath, const string& content) {
+    ofstream out(filePath);
+
+    if (!out.is_open())
+        throw runtime_error("Cannot open file to write: " + filePath);
+
+    out << content;
+}
+
+void toUpperCase(string& text) {
+    for (char& c : text) {
+        c = toupper((unsigned char)c);
+    }
+}
+
+void toLowerCase(string& text) {
+    for (char& c : text) {
+        c = tolower((unsigned char)c);
+    }
+}
